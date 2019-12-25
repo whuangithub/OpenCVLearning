288394043d8e85715381a207efdbe573087aa049
whuangithub <convolutionary@protonmail.com>
2019-12-25 23:25:16 +0900

    1225

--- mask_operation.cpp
+++ mask_operation.cpp
@@ -6,6 +6,23 @@
 using namespace std;
 using namespace cv;
 
+/*
+The idea of mask operation is that we recalculate each pixel's value in an image according to a mask matrix (kernel).
+This mask holds values that will adjust how much influence neighboring pixels (and the current pixel) have on the new pixel value.
+From a mathematical point of view we make a weighted average, with our specified values.
+example
+I(i,j)= 5∗I(i,j)−[I(i−1,j)+I(i+1,j)+I(i,j−1)+I(i,j+1)]
+⟺
+I(i,j)∗M where
+M=
+i∖j	|	−1		0		+1
+----+--------------
+ −1	|	 0		−1	 0
+	0	|	−1	  5		−1
+ +1	|  0	 −1	   0
+*/
+
+// why pointer?
 static void help(char* progName)
 {
 		cout << endl
@@ -15,16 +32,24 @@ static void help(char* progName)
 				<< progName << " [image_path -- default lena.jpg] [G -- grayscale] "        << endl << endl;
 }
 
-
+//declare fucntion Sharpen
 void Sharpen(const Mat& myImage,Mat& Result);
 
+// argc: (argument count) number of strings pointed by argv
+// argv: (argument vector) array of  pointer
 int main( int argc, char* argv[])
 {
 		help(argv[0]);
+		// “？ ：” condition operator
+		// if argc >= 2 filename = argv[1] (pointer to a char)(array of char)
+		// else filename = "lena.jpg"
+		// filename is a pointer to a char <==> a array of char
+		// so filename = "lena.jpg" / *filename = l (the first character)
 		const char* filename = argc >=2 ? argv[1] : "lena.jpg";
 
 		Mat src, dst0, dst1;
 
+		// read image gray/color
 		if (argc >= 3 && !strcmp("G", argv[2]))
 				src = imread( samples::findFile( filename ), IMREAD_GRAYSCALE);
 		else
@@ -35,6 +60,8 @@ int main( int argc, char* argv[])
 				cerr << "Can't open image ["  << filename << "]" << endl;
 				return EXIT_FAILURE;
 		}
+
+
 
 		namedWindow("Input", WINDOW_AUTOSIZE);
 		namedWindow("Output", WINDOW_AUTOSIZE);
@@ -49,6 +76,8 @@ int main( int argc, char* argv[])
 
 		imshow( "Output", dst0 );
 		waitKey();
+
+	//*********** [filter2D ]**************
 
 	//![kern]
 		Mat kernel = (Mat_<char>(3,3) <<  0, -1,  0,
@@ -69,10 +98,11 @@ int main( int argc, char* argv[])
 		waitKey();
 		return EXIT_SUCCESS;
 }
-//! [basic_method]
+//************* [basic_method] *****************
 void Sharpen(const Mat& myImage,Mat& Result)
 {
 	//! [8_bit]
+		//CV_Assert: Checks a condition at runtime and throws exception if it fails
 		CV_Assert(myImage.depth() == CV_8U);  // accept only uchar images
 	//! [8_bit]
 
@@ -84,12 +114,15 @@ void Sharpen(const Mat& myImage,Mat& Result)
 	//! [basic_method_loop]
 		for(int j = 1 ; j < myImage.rows-1; ++j)
 		{
+				// use pointer to denote different rows (array)
 				const uchar* previous = myImage.ptr<uchar>(j - 1);
 				const uchar* current  = myImage.ptr<uchar>(j    );
 				const uchar* next     = myImage.ptr<uchar>(j + 1);
 
+				// output: pointer in the Result matrix for exporting
 				uchar* output = Result.ptr<uchar>(j);
 
+				// calculate based on the mask matrix
 				for(int i= nChannels;i < nChannels*(myImage.cols-1); ++i)
 				{
 						*output++ = saturate_cast<uchar>(5*current[i]
