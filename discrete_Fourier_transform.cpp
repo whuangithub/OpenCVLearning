3763a9f4f9d7b0ce00ea49941ec3161de771e537
whuangithub <convolutionary@protonmail.com>
2019-12-27 23:34:19 +0900

    1227

--- discrete_Fourier_transform.cpp
+++ discrete_Fourier_transform.cpp
@@ -3,6 +3,9 @@
 
 // https://docs.opencv.org/master/d8/d01/tutorial_discrete_fourier_transform.html
 
+// Fourier Transform
+// transform an image from its spatial domain to its frequency domain
+
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -35,20 +38,32 @@ int main(int argc, char ** argv)
 				return EXIT_FAILURE;
 		}
 
+//The performance of a DFT is dependent of the image size.
+//It tends to be the fastest for image sizes that are multiple of the numbers two, three and five.
+//Therefore, to achieve maximal performance it is generally a good idea to pad border values to the image to get a size with such traits.
+//The getOptimalDFTSize() returns this optimal size
+// and we can use the copyMakeBorder() function to expand the borders of an image (the appended pixels are initialized with zero).
+
+//TL DR: expand the borders to accelerate the computation
+
 //! [expand]
 		Mat padded;                            //expand input image to optimal size
 		int m = getOptimalDFTSize( I.rows );
 		int n = getOptimalDFTSize( I.cols ); // on the border add zero values
+		//             src  dst   top   botom   left   right
 		copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
 //! [expand]
 
 //! [complex_and_real]
+		// fesquency domain: float
+		// complex & real 2 parts
 		Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
 		Mat complexI;
 		merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
 //! [complex_and_real]
 
 //! [dft]
+		// Discrete Fourier transform
 		dft(complexI, complexI);            // this way the result may fit in the source matrix
 //! [dft]
 
@@ -56,6 +71,7 @@ int main(int argc, char ** argv)
 		// => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
 //! [magnitude]
 		split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+		// save the magnitude (norm) to planes[0]
 		magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
 		Mat magI = planes[0];
 //! [magnitude]
@@ -67,6 +83,17 @@ int main(int argc, char ** argv)
 
 //! [crop_rearrange]
 		// crop the spectrum, if it has an odd number of rows or columns
+		// bitwise and with -2
+		// There are three steps necessary to convert a negative decimal integer to two’s complement form:
+		// 1. Start with the positive binary value, expanded to fill the number of bits you will be storing the number into.
+		// 0000000000010
+		// 2. Complement/flip all of the bits. This means all of the 1s become 0s and all of the 0s become 1s
+		// 1111111111101
+		// 3. Add one to the flipped bits.
+		// 1111111111110
+
+		// -1 twice from 0000000000000
+
 		magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
 
 		// rearrange the quadrants of Fourier image  so that the origin is at the image center
